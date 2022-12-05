bcca667fb304c3ee469515394746f28ceb2aff46
danielsafari143 <danielsafari143@gmail.com>
2022-12-05 18:39:13 +0100

    last commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Books = [];
 function Library(books) {
   this.books = books;
-  this.display = function display(arg = null) {
-    if (arg === null) {
-      for (let i = 0; i < Books.length; i += 1) {
+  this.display = function display(initialObj, arg) {
+    if (arg === null && initialObj !== null) {
+      for (let i = 0; i < initialObj.length; i += 1) {
         document.getElementById('books').innerHTML += `
-            <h3>${this.books[i].author}</h3>
-            <p>${this.books[i].title}</p>
+            <h3>${initialObj[i].author}</h3>
+            <p>${initialObj[i].title}</p>
             <button id='remove' onClick='bi.remove(${i})'>Remove</button>
             <hr>
             `;
@@ -15,8 +15,8 @@ function Library(books) {
       document.getElementById('books').innerHTML = '';
       for (let i = 0; i < arg.length; i += 1) {
         document.getElementById('books').innerHTML += `
-          <h3>${this.books[i].author}</h3>
-          <p>${this.books[i].title}</p>
+          <h3>${arg[i].author}</h3>
+          <p>${arg[i].title}</p>
           <button id='remove' onClick='bi.remove(${i})'>Remove</button>
           <hr>
           `;
@@ -25,18 +25,25 @@ function Library(books) {
   };
 
   this.remove = function remove(ref) {
+    this.books = JSON.parse(localStorage.getItem('datas'));
     this.books.splice(ref, 1);
-    this.display(this.books);
+    localStorage.setItem('datas', JSON.stringify(this.books));
+    this.display(null, JSON.parse(localStorage.getItem('datas')));
+    this.books = JSON.parse(localStorage.getItem('datas'));
   };
 
   this.add = function add(author, title) {
     const hauteur = document.getElementById(author).value;
     const titre = document.getElementById(title).value;
     if (titre !== '' && hauteur !== '') {
+      this.books = JSON.parse(localStorage.getItem('datas'));
       this.books.push({ author: hauteur, title: titre });
-      this.display(this.books);
+      localStorage.setItem('datas', JSON.stringify(this.books));
+      document.getElementById(author).value = '';
+      document.getElementById(title).value = '';
+      this.display(null, JSON.parse(localStorage.getItem('datas')));
     }
   };
 }
 const bi = new Library(Books);
-bi.display();
+bi.display(JSON.parse(localStorage.getItem('datas')), null);
